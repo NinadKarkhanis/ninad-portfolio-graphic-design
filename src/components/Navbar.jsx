369f5141ab98a2f05868d903f732a2df41b5ef77
Ninad <ninadkarkhanis2003@gmail.com>
2024-07-31 12:52:26 +0530

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import logo from "../assets/logo.png";
 import { FaLinkedin } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 
@@ -6,7 +6,7 @@ import { FaGithub } from 'react-icons/fa';
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div className='flex flex-shrink-0 items-center'>
-       
+     <img className="mx-2 w-10" src={logo} alt="logo"></img>
     </div>
     <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
         <a  href='https://www.linkedin.com/in/ninad-karkhanis-121a7625a'><FaLinkedin ></FaLinkedin></a>
