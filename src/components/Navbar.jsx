433bc14606af2cc4bb946c6441cb100c3de1fa16
Ninad <ninadkarkhanis2003@gmail.com>
2024-07-31 16:03:55 +0530

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,14 +15,59 @@ const Navbar = () => {
     <div className='flex flex-shrink-0 items-center'>
      <img className="mx-2 w-10" src={logo} alt="logo"></img>
     </div>
-    <div className="flex space-x-9 text-neutral-400">
-     <Link className="cursor-pointer hover:text-purple-300 hover:glow" to="about" smooth={true} duration={500}> About Me </Link>
-     <Link className="cursor-pointer hover:text-purple-300 hover:glow" to="edu" smooth={true} duration={500}> Education </Link>
-     <Link className="cursor-pointer hover:text-purple-300 hover:glow" to="exp" smooth={true} duration={500}> Work Experience </Link>
-     <Link className="cursor-pointer hover:text-purple-300 hover:glow" to="tech" smooth={true} duration={500}> Technologies </Link>
-     <Link className="cursor-pointer hover:text-purple-300 hover:glow" to="proj" smooth={true} duration={500}> Projects </Link>
-     <Link className="cursor-pointer hover:text-purple-300 hover:glow" to="con" smooth={true} duration={500}> Contact Me </Link>
-    </div>
+    <div className="m-4 sm:m-6 md:m-8 lg:m-10 flex flex-wrap items-center justify-center gap-4 text-neutral-400">
+  <Link 
+    className="cursor-pointer hover:text-purple-300 hover:glow" 
+    to="about" 
+    smooth={true} 
+    duration={500}
+  >
+    About Me
+  </Link>
+  <Link 
+    className="cursor-pointer hover:text-purple-300 hover:glow" 
+    to="edu" 
+    smooth={true} 
+    duration={500}
+  >
+    Education
+  </Link>
+  <Link 
+    className="cursor-pointer hover:text-purple-300 hover:glow" 
+    to="exp" 
+    smooth={true} 
+    duration={500}
+  >
+    Work Experience
+  </Link>
+  <Link 
+    className="cursor-pointer hover:text-purple-300 hover:glow" 
+    to="tech" 
+    smooth={true} 
+    duration={500}
+  >
+    Technologies
+  </Link>
+  <Link 
+    className="cursor-pointer hover:text-purple-300 hover:glow" 
+    to="proj" 
+    smooth={true} 
+    duration={500}
+  >
+    Projects
+  </Link>
+  <Link 
+    className="cursor-pointer hover:text-purple-300 hover:glow" 
+    to="con" 
+    smooth={true} 
+    duration={500}
+  >
+    Contact Me
+  </Link>
+</div>
+
+
+
     <div className='m-8 flex items-center justify-center gap-4 text-2xl  text-neutral-400'>
         <a className="hover:text-purple-300 " href='https://www.linkedin.com/in/ninad-karkhanis-121a7625a'><FaLinkedin ></FaLinkedin></a>
         <a className=" hover:text-purple-300" href='https://github.com/NinadKarkhanis'><FaGithub ></FaGithub></a>
